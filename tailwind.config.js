f8f53de4b54097bce979321767add25374e9b899
Kofo <kajotonis@gmail.com>
2022-02-20 18:38:19 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container:{
+      center: true,
+     },
+    extend: {
+      fontFamily: {
+      'Bellefair': ['Bellefair'],
+      'Barlow': ['Barlow'],
+      'BarlowCondensed': ['Barlow Condensed']
+      },
+      colors: {
+        'palepurple': '#D0D6F9',
+      },
+    },
+  },
+  plugins: [],
+}
